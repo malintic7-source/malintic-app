a78fff3c28e1f6c5610453ec5d809a3c38b5c1d5
mamadoutoure3648-source <malintic7@gmail.com>
2026-08-24 12:06:30 +0000

    Configure Vercel Serverless API routing

--- api/index.js
+++ api/index.js
@@ -242,7 +242,7 @@ const inscriptionAttempts = new Map();
 const INSCRIPTION_MAX_PER_HOUR = 10;
 const INSCRIPTION_WINDOW_MS = 60 * 60 * 1000;
 
-export default function handler(req, res) {
+module.exports = function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
